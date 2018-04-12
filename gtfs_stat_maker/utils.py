3d6218612d99b6438b88d196e647a73a2573c745
Drew Cooper <sdrewc@gmail.com>
2018-04-12 15:40:56 -0700

    add calc_headway, str_to_timedelta, and apply_time_periods.  Remove apply_calc_runtime and apply_calc_movetime because apply_diff makes them unnecessary
    Signed-off-by: Drew Cooper <sdrewc@gmail.com>

--- gtfs_stat_maker/utils.py
+++ gtfs_stat_maker/utils.py
@@ -37,14 +37,18 @@ def stdtime(series, ignore_date=True, normalize=True, zero_hour=3):
         series = normalize_timedelta(series, zero_hour)
     return series.std()
 
+def calc_headway(series, periods=-1):
+    diff = series.shift(periods) - series
+    return diff.mean()
+
 def apply_diff(x, val1, val2):
     return x[val1] - x[val2]
 
-def apply_calc_runtime(x):
-    return x['last_arrival_time'] - x['first_arrival_time']
-
-def apply_calc_movetime(x): 
-    return x['observed_runtime'] - x['observed_stopped_time']
+#def apply_calc_runtime(x):
+#    return x['last_arrival_time'] - x['first_arrival_time']
+#
+#def apply_calc_movetime(x): 
+#    return x['observed_runtime'] - x['observed_stopped_time']
         
 def normalize_timedelta(series, zero_hour=0):
     '''
@@ -61,6 +65,24 @@ def normalize_timedelta(series, zero_hour=0):
         return shifted
     else:
         return series
+
+#def tpmap(df, timefield='ARRIVAL_TIME', tpfield='time_period', tpnames=['ea','am','md','pm','ev'], tpranges=[[dt.time(3),dt.time(6)],[dt.time(6),dt.time(9)],[dt.time(9),dt.time(15,30)],[dt.time(15,30),dt.time(18,30)],[dt.time(18,30),]):
+#    df.loc[df[timefield].]
+def str_to_timedelta(hhmmss):
+    split = hhmmss.split(':')
+    return dt.timedelta(hours=int(split[0]) % 24, minutes=int(split[1]), seconds=int(split[2]))
+
+def apply_time_periods(series, timeperiods):
+    timeperiods.sort_values(by='timeperiod_start_time')
+    tpseries = pd.Series(index=series.index)
+    for idx, row in timeperiods.iterrows():
+        if row['timeperiod_start_time'] < row['timeperiod_end_time']:
+            tpseries.loc[(series>row['timeperiod_start_time']) & (series<row['timeperiod_end_time'])] = row['timeperiod_id']
+        else:
+            tpseries.loc[(series<row['timeperiod_end_time']) | (series>row['timeperiod_start_time'])] = row['timeperiod_id']
+    return tpseries
+
+#def calc_headway(series)
 
 def datetime_to_timedelta(d):
         try:
